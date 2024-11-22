abe589f81253abff5edee3908e2ef6091818c963
Hyria Roman <hyrya.roman@gmail.com>
2024-11-22 17:42:03 +0200

    Fetch properties

---
+++ src/assets/ts/local_properties.ts
@@ -0,0 +1,63 @@
+{
+  type PropertyInfo = {
+    image: string,
+    title: string,
+    price: number,
+    location: string,
+    bedroom_count: number,
+    bathroom_count: number,
+    area: number
+  };
+
+  const formatter = new Intl.NumberFormat(undefined, {
+    style: 'decimal',
+    currency: 'USD',
+    signDisplay: 'never',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  const propertyCardTemplate = document.getElementById("property-card-template") as HTMLTemplateElement;
+
+  function createCardFromInfo(info: PropertyInfo): HTMLElement {
+    const card = propertyCardTemplate.content.cloneNode(true) as HTMLElement;
+
+    const slots = card.querySelectorAll("[data-source]");
+
+    slots.forEach((e) => {
+      const source = e.getAttribute("data-source") as string;
+      const target = e.getAttribute("data-target") || "innerHTML";
+
+      let value = info[source];
+
+      // Format price according to user locale
+      if (source === "price") {
+        value = formatter.format(value);
+      }
+
+      e[target] = value;
+    });
+
+    return card;
+  }
+
+  function createCards(properties: [PropertyInfo]) {
+    const list = document.getElementById("propertiesList") as HTMLElement;
+    properties.forEach((info) => {
+      list.appendChild(createCardFromInfo(info));
+    });
+  }
+
+  function responseToJson(v: Response) {
+    return v.json();
+  }
+
+  function onFetchError(e: any) {
+    console.error("Error:", e);
+  }
+
+  fetch('assets/samples/local_properties')
+    .then(responseToJson)
+    .then(createCards)
+    .catch(onFetchError);
+}
